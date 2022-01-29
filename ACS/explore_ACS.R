5f94e2768e288c40287ffbe9d3102d4f8620452f
CHEN Dongmei <DCHEN@lcog.org>
2022-01-29 00:34:46 -0800

    explored ACS

--- ACS/explore_ACS.R
+++ ACS/explore_ACS.R
@@ -55,23 +55,51 @@ for(file in files){
 df_eug <- df
 
 data <- df_total
+
+# percent
 data_means <- aggregate(data$PCT,                       
                         list(data$YEAR),
                         function(x) mean(na.omit(x)))
 
 colnames(data_means)[1] <- "Year"
 
+data_maxes <- aggregate(data$PCT,                       
+                        list(data$YEAR),
+                        function(x) max(na.omit(x)))
+
+colnames(data_maxes)[1] <- "Year"
+
 for(year in 2010:2019){
   sdf <- data[data$YEAR==year,]
   max <- max(na.omit(sdf$PCT))
-  print(paste(year, ": ", sdf[sdf$PCT==max,"NAME"]))
+  print(paste0(year, ": ", sdf[sdf$PCT==max,"NAME"]))
 }
+
+data_means_tot <- aggregate(data$B08301_018E,                       
+                        list(data$YEAR),
+                        function(x) mean(na.omit(x)))
+
+colnames(data_means_tot)[1] <- "Year"
+
+data_maxes_tot <- aggregate(data$B08301_018E,                       
+                        list(data$YEAR),
+                        function(x) max(na.omit(x)))
+
+colnames(data_maxes_tot)[1] <- "Year"
+
+for(year in 2010:2019){
+  sdf <- data[data$YEAR==year,]
+  max <- max(na.omit(sdf$B08301_018E))
+  print(paste0(year, ": ", sdf[sdf$B08301_018E==max,"NAME"]))
+}
+
 
 png(paste0(inpath, "/boxplot_bike_commuters.png"), width = 8, height = 6,
     units = "in", res = 300)
 par(mar=c(2,4,1,1))
 boxplot(df_total$PCT~df_total$YEAR, xlab="", 
-        ylab="% Bike Commuters")
+        ylab="% Bike Commuters",
+        col="grey", outcol="lightgrey")
 par(new=T)
 plot(df_eug$YEAR, df_eug$PCT, xlim=c(2009.5, 2019.5), 
      ylim=range(na.omit(df_total$PCT)), pch=19, cex=1.5,
@@ -83,14 +111,35 @@ points(x = data_means$Year,
        col = "blue",
        pch = 16)
 text(x = data_means$Year,                                
-     y = data_means$x + 0.0012,
-     labels = round(data_means$x,4),
+     y = data_means$x + 0.002,
+     labels = paste0(round(data_means$x,4)*100, "%"),
      col = "blue")
 text(x = df_eug$YEAR,                               
      y = df_eug$PCT+0.002,
-     labels = round(df_eug$PCT,4),
+     labels = paste0(round(df_eug$PCT,4)*100, "%"),
      col = "red")
-text(x = 2016,                               
-     y = 0.08924934-0.001,
+points(x = data_maxes$Year[2:10],                             
+       y = data_maxes$x[2:10])
+lines(data_maxes$Year[2:10], data_maxes$x[2:10], 
+      lwd=2, lty=2)
+text(x = 2018,                               
+     y = 0.085,
      labels = "Corvallis")
 dev.off()
+
+
+boxplot(df_total$B08301_018E~df_total$YEAR, xlab="", 
+        ylab="No. Bike Commuters",
+        col="grey", outcol="lightgrey")
+par(new=T)
+plot(df_eug$YEAR, df_eug$B08301_018E, xlim=c(2009.5, 2019.5), 
+     ylim=range(na.omit(df_total$B08301_018E)), pch=19, cex=1.5,
+     col="red", axes=F, xlab="", ylab="")
+lines(df_eug$YEAR, df_eug$B08301_018E, 
+      lwd=2, lty=2, col="red")
+
+mean(df_total[grepl("Eugene", df_total$NAME), "B08301_001E"])
+mean(df_total[grepl("Corvallis", df_total$NAME), "B08301_001E"])
+
+mean(df_total[grepl("Eugene", df_total$NAME), "B08301_018E"])
+mean(df_total[grepl("Corvallis", df_total$NAME), "B08301_018E"])
