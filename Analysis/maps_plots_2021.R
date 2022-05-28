3bd441d3a6eb632163e1749fda6ee4db6b433270
CHEN Dongmei <DCHEN@lcog.org>
2022-05-28 00:45:53 -0700

    updated bike counts

---
+++ Analysis/maps_plots_2021.R
@@ -0,0 +1,19 @@
+
+options(warn = -1)
+library(rgdal)
+
+BikeCounts <- readOGR(dsn="T:/DCProjects/StoryMap/BikeCounting/BikeCounts/Output", layer="Mean_Daily_Bike_Counts_2021")
+BikeShare <- readOGR(dsn="T:/DCProjects/StoryMap/BikeCounting/BikeShare/Output", layer="Sum_Bike_Share_Trips")
+BikesOnBuses <- readOGR(dsn="T:/DCProjects/StoryMap/BikeCounting/BikeOnBuses/Output", layer="Sum_Bikes_On_Buses")
+
+countsCut <- 521
+shareCut <- 21.3
+BOBCut <- 2.15
+
+
+MPOBound <- readOGR(dsn = "V:/Data/Transportation", layer="MPO_Bound")
+plot(MPOBound)
+plot(BikeCounts[BikeCounts$AvDlyCn >= countsCut,], col='blue', cex=2, add=T)
+plot(BikeShare[BikeShare$AvgNoTrips >= shareCut,], col='red', pch=2, cex=2, add=T)
+plot(BikesOnBuses[BikesOnBuses$avgqty >= BOBCut,], col='green', pch=1, cex=2, add=T)
+legend()
