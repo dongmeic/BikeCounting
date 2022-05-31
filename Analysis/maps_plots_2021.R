028f0ab0f069ec1bc572a5be7d20a85f76ff67e2
CHEN Dongmei <DCHEN@lcog.org>
2022-05-31 11:02:11 -0700

    high counts loc

--- Analysis/maps_plots_2021.R
+++ Analysis/maps_plots_2021.R
@@ -12,8 +12,16 @@ BOBCut <- 2.15
 
 
 MPOBound <- readOGR(dsn = "V:/Data/Transportation", layer="MPO_Bound")
+
+outpath <- "T:/DCProjects/StoryMap/BikeCounting/figures"
+png(paste0(outpath, "/high_bike_counts_loc.png"), width = 6, height = 5,
+    units = "in", res = 300)
+par(mar=c(1,1,1,1))
 plot(MPOBound)
 plot(BikeCounts[BikeCounts$AvDlyCn >= countsCut,], col='blue', cex=2, add=T)
 plot(BikeShare[BikeShare$AvgNoTrips >= shareCut,], col='red', pch=2, cex=2, add=T)
 plot(BikesOnBuses[BikesOnBuses$avgqty >= BOBCut,], col='green', pch=1, cex=2, add=T)
-legend()
+legend("topright", c("Bike Counts", "Bike Share", "Bikes on Buses"), 
+       pch = c(3, 2, 1), col=c("blue", "red", "green"),
+       cex = rep(1.5,3))
+dev.off()
